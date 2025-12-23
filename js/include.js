b817fbce422fc419fd9df69af402ec40702ec4a3
김슬기 <xxx696193@gmail.com>
2025-12-23 17:38:13 +0900

    1223

--- js/include.js
+++ js/include.js
@@ -1,22 +1,17 @@
 $(document).ready(function () {
   const leftSidebar = `
-      <ul class="user_menu">
-        <li><a href="/basic/signup.html">Sign Up</a></li>
-        <li><a href="/basic/signin.html">Sign in</a></li>
-      </ul>
-
       <button id="left_sidebar_toggle">☰</button>
 
       <div class="ico_nav">
         <a href="/basic/signin.html"><img src="/assets/img/ico-User.svg" alt="ico" title="Sign in"></a>
-        <a href="index.html"><img src="/assets/img/ico-Home.svg" alt="ico" title="Home"></a>
+        <a href="../index.html"><img src="/assets/img/ico-Home.svg" alt="ico" title="Home"></a>
         <a href="/basic/foryou.html"><img src="/assets/img/ico-For-you.svg" alt="ico" title="For you"></a>
         <a href="/basic/library.html"><img src="/assets/img/ico-Library.svg" alt="ico" title="Library"></a>
         <a href="/basic/upgrade.html"><img src="/assets/img/ico-Upgrade.svg" alt="ico" title="Upgrade"></a>
       </div>
 
       <nav class="nav">
-        <a class="active" href="index.html">Home</a>
+        <a class="active" href="../index.html">Home</a>
         <a href="/basic/foryou.html">For you</a>
         <a href="/basic/library.html">Library</a>
         <a href="/basic/upgrade.html">Upgrade</a>
@@ -24,16 +19,28 @@ $(document).ready(function () {
   `;
 
   $(".left_sidebar").append(leftSidebar);
+  const top = `
+        <input class="search" placeholder="Search" />
+        <div>
+          <ul class="user_menu">
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Sign In</a></li>
+          </ul>
+        </div>
+  `;
 
-  $(document).on("click", "#left_sidebar_toggle", function () {
-    $(".left_sidebar").toggleClass("is_collapsed");
-    $(".layout").toggleClass("sidebar_collapsed");
-    $(this).text($(this).text() === "☰" ? "✕" : "☰");
-  });
+  $(".top").append(top);
 
-  $(document).on("click", ".left_sidebar .nav a", function () {
-    $(".left_sidebar .nav a").removeClass("active");
-    $(this).addClass("active");
-  });
+});
 
+
+$(document).on("click", "#left_sidebar_toggle", function () {
+  $(".left_sidebar").toggleClass("is_collapsed");
+  $(".layout").toggleClass("sidebar_collapsed");
+  $(this).text($(this).text() === "☰" ? "✕" : "☰");
+});
+
+$(document).on("click", ".left_sidebar .nav a", function () {
+  $(".left_sidebar .nav a").removeClass("active");
+  $(this).addClass("active");
 });
